7c1a697f0f59f6d17201c7157d2933fe1d6cfd2e
guminskaya484@gmail.com <guminskaya484@gmail.com>
2024-10-23 11:55:24 +0300

    Добавьте файлы проекта.

---
+++ GraphApp/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace GraphApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAddEdge;
+        private System.Windows.Forms.Button btnFindShortestPath;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.Panel panelGraph; // Новый элемент для рисования графа
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAddEdge = new System.Windows.Forms.Button();
+            this.btnFindShortestPath = new System.Windows.Forms.Button();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.panelGraph = new System.Windows.Forms.Panel();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(400, 300);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAddEdge
+            // 
+            this.btnAddEdge.Location = new System.Drawing.Point(420, 140);
+            this.btnAddEdge.Name = "btnAddEdge";
+            this.btnAddEdge.Size = new System.Drawing.Size(155, 23);
+            this.btnAddEdge.TabIndex = 7;
+            this.btnAddEdge.Text = "Добавить ребро";
+            this.btnAddEdge.UseVisualStyleBackColor = true;
+            this.btnAddEdge.Click += new System.EventHandler(this.btnAddEdge_Click);
+            // 
+            // btnFindShortestPath
+            // 
+            this.btnFindShortestPath.Location = new System.Drawing.Point(420, 250);
+            this.btnFindShortestPath.Name = "btnFindShortestPath";
+            this.btnFindShortestPath.Size = new System.Drawing.Size(155, 23);
+            this.btnFindShortestPath.TabIndex = 12;
+            this.btnFindShortestPath.Text = "Найти кратчайший путь";
+            this.btnFindShortestPath.UseVisualStyleBackColor = true;
+            this.btnFindShortestPath.Click += new System.EventHandler(this.btnFindShortestPath_Click);
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(475, 27);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(100, 22);
+            this.txtFrom.TabIndex = 2;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(475, 67);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(100, 22);
+            this.txtTo.TabIndex = 4;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(475, 107);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 22);
+            this.txtWeight.TabIndex = 6;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(475, 177);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 22);
+            this.txtStart.TabIndex = 9;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(475, 217);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 22);
+            this.txtEnd.TabIndex = 11;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(420, 280);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 16);
+            this.lblResult.TabIndex = 13;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(420, 30);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(28, 16);
+            this.lblFrom.TabIndex = 1;
+            this.lblFrom.Text = "Из:";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(420, 70);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(19, 16);
+            this.lblTo.TabIndex = 3;
+            this.lblTo.Text = "В:";
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(420, 110);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(34, 16);
+            this.lblWeight.TabIndex = 5;
+            this.lblWeight.Text = "Вес:";
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Location = new System.Drawing.Point(420, 180);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(49, 16);
+            this.lblStart.TabIndex = 8;
+            this.lblStart.Text = "Старт:";
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Location = new System.Drawing.Point(420, 220);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(50, 16);
+            this.lblEnd.TabIndex = 10;
+            this.lblEnd.Text = "Конец:";
+            // 
+            // panelGraph
+            // 
+            this.panelGraph.Location = new System.Drawing.Point(12, 320);
+            this.panelGraph.Name = "panelGraph";
+            this.panelGraph.Size = new System.Drawing.Size(550, 300);
+            this.panelGraph.TabIndex = 14;
+            this.panelGraph.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGraph_Paint);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Из";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "В";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Вес";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 125;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(612, 631);
+            this.Controls.Add(this.panelGraph);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnFindShortestPath);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.btnAddEdge);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Поиск кратчайшего пути в графе";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+    }
+}
---
+++ GraphApp/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GraphApp
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<Edge>> graph = new Dictionary<string, List<Edge>>();
+        private int nodeCount = 0; // Для отслеживания количества вершин
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Добавление ребра
+        private void btnAddEdge_Click(object sender, EventArgs e)
+        {
+            string from = txtFrom.Text;
+            string to = txtTo.Text;
+            int weight;
+
+            if (!int.TryParse(txtWeight.Text, out weight))
+            {
+                MessageBox.Show("Вес должен быть числом.");
+                return;
+            }
+
+            // Добавляем вершину "from" (исходную), если она ещё не существует
+            if (!graph.ContainsKey(from))
+            {
+                graph[from] = new List<Edge>();
+                nodeCount++; // Увеличиваем количество вершин
+            }
+
+            // Добавляем вершину "to" (конечную), если она ещё не существует
+            if (!graph.ContainsKey(to))
+            {
+                graph[to] = new List<Edge>();
+                nodeCount++; // Увеличиваем количество вершин
+            }
+
+            // Добавляем ребро в обоих направлениях, так как это неориентированный граф
+            graph[from].Add(new Edge(to, weight));
+            graph[to].Add(new Edge(from, weight));
+
+            // Обновляем таблицу
+            dataGridView1.Rows.Add(from, to, weight);
+
+            // Перерисовать граф
+            panelGraph.Invalidate();
+        }
+
+        // Нахождение кратчайшего пути с помощью алгоритма Дейкстры
+        private void btnFindShortestPath_Click(object sender, EventArgs e)
+        {
+            string start = txtStart.Text;
+            string end = txtEnd.Text;
+
+            var result = Dijkstra(start, end);
+            lblResult.Text = result == int.MaxValue ? "Нет пути" : $"Кратчайший путь: {result}";
+        }
+
+        // Алгоритм Дейкстры
+        private int Dijkstra(string start, string end)
+        {
+            var distances = new Dictionary<string, int>();
+            var previousNodes = new Dictionary<string, string>();
+            var nodes = new List<string>(graph.Keys);
+
+            foreach (var node in nodes)
+            {
+                distances[node] = int.MaxValue;
+                previousNodes[node] = null;
+            }
+            distances[start] = 0;
+
+            while (nodes.Count > 0)
+            {
+                nodes.Sort((x, y) => distances[x] - distances[y]);
+                var smallest = nodes[0];
+                nodes.Remove(smallest);
+
+                if (smallest == end)
+                {
+                    break;
+                }
+
+                if (distances[smallest] == int.MaxValue)
+                {
+                    break;
+                }
+
+                foreach (var edge in graph[smallest])
+                {
+                    int alt = distances[smallest] + edge.Weight;
+                    if (alt < distances[edge.To])
+                    {
+                        distances[edge.To] = alt;
+                        previousNodes[edge.To] = smallest;
+                    }
+                }
+            }
+
+            return distances.ContainsKey(end) ? distances[end] : int.MaxValue;
+        }
+
+        // Отрисовка графа
+        private void panelGraph_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int nodeRadius = 20; // Радиус узлов
+            Dictionary<string, Point> nodePositions = new Dictionary<string, Point>();
+
+            // Задаём центр и радиус круга для размещения вершин
+            int centerX = panelGraph.Width / 2;
+            int centerY = panelGraph.Height / 2;
+            int circleRadius = Math.Min(centerX, centerY) - 50; // Радиус круга
+
+            // Угол между соседними вершинами (в радианах)
+            double angleStep = 2 * Math.PI / nodeCount;
+
+            // Размещаем вершины по окружности
+            int i = 0;
+            foreach (var node in graph.Keys)
+            {
+                double angle = i * angleStep; // Угол для текущей вершины
+                int x = centerX + (int)(circleRadius * Math.Cos(angle)); // Координата X
+                int y = centerY + (int)(circleRadius * Math.Sin(angle)); // Координата Y
+                nodePositions[node] = new Point(x, y);
+                i++;
+            }
+
+            Pen edgePen = new Pen(Color.Black, 2);
+
+            // Рисуем рёбра (линии) без стрелок, так как граф неориентированный
+            foreach (var from in graph.Keys)
+            {
+                foreach (var edge in graph[from])
+                {
+                    if (nodePositions.TryGetValue(from, out Point fromPos) && nodePositions.TryGetValue(edge.To, out Point toPos))
+                    {
+                        // Проверяем, не нарисовано ли уже обратное ребро (т.е. ребро от "to" к "from")
+                        if (from.CompareTo(edge.To) < 0)
+                        {
+                            // Вычисляем направление от "from" к "to"
+                            double dx = toPos.X - fromPos.X;
+                            double dy = toPos.Y - fromPos.Y;
+                            double distance = Math.Sqrt(dx * dx + dy * dy);
+
+                            // Вычисляем новые координаты, чтобы линия не пересекала круги вершин
+                            int edgeOffset = nodeRadius; // Смещение линии на радиус узла
+                            Point adjustedFromPos = new Point(
+                                fromPos.X + (int)(edgeOffset * dx / distance),
+                                fromPos.Y + (int)(edgeOffset * dy / distance)
+                            );
+                            Point adjustedToPos = new Point(
+                                toPos.X - (int)(edgeOffset * dx / distance),
+                                toPos.Y - (int)(edgeOffset * dy / distance)
+                            );
+
+                            // Рисуем ребро
+                            g.DrawLine(edgePen, adjustedFromPos, adjustedToPos);
+
+                            // Подписываем вес ребра в середине линии
+                            Point midPoint = new Point((adjustedFromPos.X + adjustedToPos.X) / 2, (adjustedFromPos.Y + adjustedToPos.Y) / 2);
+                            g.DrawString(edge.Weight.ToString(), this.Font, Brushes.Red, midPoint);
+                        }
+                    }
+                }
+            }
+
+            // Рисуем узлы (круги)
+            foreach (var node in nodePositions)
+            {
+                Point nodePos = node.Value;
+                g.FillEllipse(Brushes.Blue, nodePos.X - nodeRadius, nodePos.Y - nodeRadius, nodeRadius * 2, nodeRadius * 2);
+                g.DrawEllipse(edgePen, nodePos.X - nodeRadius, nodePos.Y - nodeRadius, nodeRadius * 2, nodeRadius * 2);
+
+                // Подписываем узел (имя вершины)
+                g.DrawString(node.Key, this.Font, Brushes.White, nodePos.X - 10, nodePos.Y - 10);
+            }
+        }
+    }
+
+    // Класс рёбер
+    public class Edge
+    {
+        public string To { get; }
+        public int Weight { get; }
+
+        public Edge(string to, int weight)
+        {
+            To = to;
+            Weight = weight;
+        }
+    }
+}
